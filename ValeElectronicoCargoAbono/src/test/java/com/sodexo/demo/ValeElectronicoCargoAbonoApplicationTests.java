8aae9d17ff77fc65297a0fe1db6efe3d7efe0e0a
Michael <kordero182@gmail.com>
2020-11-08 17:12:50 -0600

    initial commit

---
+++ ValeElectronicoCargoAbono/src/test/java/com/sodexo/demo/ValeElectronicoCargoAbonoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sodexo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValeElectronicoCargoAbonoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
