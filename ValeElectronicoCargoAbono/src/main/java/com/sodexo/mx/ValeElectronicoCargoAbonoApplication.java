702a70751b282b4abd51669cf1d6c2038dd3f5c4
Eder Gonzalez <eder.gonzalez.ext@sodexo.com>
2020-11-10 23:17:50 -0600

    antes de apache

--- ValeElectronicoCargoAbono/src/main/java/com/sodexo/mx/ValeElectronicoCargoAbonoApplication.java
+++ ValeElectronicoCargoAbono/src/main/java/com/sodexo/mx/ValeElectronicoCargoAbonoApplication.java
@@ -1,73 +1,173 @@
 package com.sodexo.mx;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
+
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.FileHandler;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.dao.DataAccessException;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.ResultSetExtractor;
 
+import com.sodexo.mx.buildQuerys.ProcessInfo;
 import com.sodexo.mx.dbconnectios.CreateConnectionDB;
-
+import com.sodexo.mx.dto.SevCargaAbonoDTO;
+import com.sodexo.mx.pojo.ObjQuery;
+import com.sodexo.mx.restservices.OperationsApiRest;
+import com.sodexo.mx.utils.Constants;
 
 @SpringBootApplication
-public class ValeElectronicoCargoAbonoApplication implements CommandLineRunner {
-	@Autowired
-    JdbcTemplate jdbcTemplate;
-	
-	
-	
-	public static void main(String[] args) {
+public class ValeElectronicoCargoAbonoApplication {
+	// @Autowired
+	// JdbcTemplate jdbcTemplate;
+	private final static Logger LOGGER = Logger.getLogger("log");
+
+	public static void main(String[] args) throws IOException, SQLException {
 		SpringApplication.run(ValeElectronicoCargoAbonoApplication.class, args);
-			//CreateConnectionDB  conn = new CreateConnectionDB();
-			//conn.testQuery();
-	
-		
+
+		executeProcess();
+
 	}
-	
 
-	
-	
-	
-	public void run(String... args) throws Exception {
-        System.out.println("Consulta");        
-        jdbcTemplate.query("select NID_CA,NCUENTA,NTARJETA,NCLIENTE,VMOTIVO,VMONTO,VCLAVE_MOV,VCARGO_ABONO,to_date(PROXIMA_EJECUCION,'dd/mm/yyyy')PROXIMA_EJECUCION,NINTENTO,NESTATUS,NMAX_INTENTOS,DFECHA_FIN,VPERIODICIDAD,VTIPO_POLIZA,NPRODUCTO from  SEV_VCARGA_ABONO where rownum <=10", (rs)-> {
-        //System.out.printf("%-30.30s  %-30.30s%n %-30.30s%n", NID= rs.getString("NID_CA"), rs.getString("NCUENTA"), rs.getString("NTARJETA")); 	
-        	
-        	String NID_CA= rs.getString("NID_CA");
-        	String NCUENTA= rs.getString("NCUENTA");
-        	String NTARJETA= rs.getString("NTARJETA");
-        	String NCLIENTE= rs.getString("NCLIENTE");
-        	String VMOTIVO= rs.getString("VMOTIVO");
-        	String VMONTO= rs.getString("VMONTO");
-        	String VCLAVE_MOV= rs.getString("VCLAVE_MOV");
-        	String VCARGO_ABONO= rs.getString("VCARGO_ABONO");
-        	String PROXIMA_EJECUCION= rs.getString("PROXIMA_EJECUCION");
-        	String NINTENTO= rs.getString("NINTENTO");
-        	String NESTATUS= rs.getString("NESTATUS");
-        	String NMAX_INTENTOS= rs.getString("NMAX_INTENTOS");
-        	String DFECHA_FIN= rs.getString("DFECHA_FIN");
-        	String VPERIODICIDAD= rs.getString("VPERIODICIDAD");
-        	String VTIPO_POLIZA= rs.getString("VTIPO_POLIZA");
-        	String NPRODUCTO= rs.getString("NPRODUCTO");
-	  	
-	  	System.out.printf( "NID_CA"+NID_CA);   
-	  	
-    
-        });
-    }
-	
+	public static void executeProcess() throws SecurityException, IOException {
 
-	
+		Handler consoleHandler = new ConsoleHandler();
+		Handler fileHandler = new FileHandler("../log/Log_valeElectronicoCargosAbonos.log", true);
+		SimpleFormatter simpleFormatter = new SimpleFormatter();
+		fileHandler.setFormatter(simpleFormatter);
+		LOGGER.addHandler(consoleHandler);
+		LOGGER.addHandler(fileHandler);
 
-	
-	
-	
+		Properties mainProperties = new Properties();
+		FileInputStream file;
+		String path = "../config/config.properties";
+		file = new FileInputStream(path);
+		mainProperties.load(file);
+		file.close();
+		String user = mainProperties.getProperty("usuario_api");
+		String password = mainProperties.getProperty("pass_api");
+		String urlSycDep = mainProperties.getProperty("url_syc_dep");
+		String urlSycRetiro = mainProperties.getProperty("url_syc_retiro");
+		String idAplicacion = mainProperties.getProperty("idAplicacion");
+		String urlSycToken = mainProperties.getProperty("url_syc_token");
+
+		int contador=0;
+		OperationsApiRest operation = new OperationsApiRest();
+		String token = operation.getToken(user, password, urlSycToken);
+
+		try (Connection conn = new CreateConnectionDB().openConnectionOracle(); Statement st = conn.createStatement()) {
+
+			List<ObjQuery> lstQuerys = new ArrayList<ObjQuery>();
+			ProcessInfo processInfo = new ProcessInfo();
+
+			ResultSet rs = st.executeQuery(
+					"select NID_CA,NCUENTA,NTARJETA,NCLIENTE,VMOTIVO,VMONTO,VCLAVE_MOV,VCARGO_ABONO,to_date(PROXIMA_EJECUCION,'dd/mm/yyyy')PROXIMA_EJECUCION,NINTENTO,NESTATUS,NMAX_INTENTOS,DFECHA_FIN,VPERIODICIDAD,VTIPO_POLIZA,NPRODUCTO from SEV_VCARGA_ABONO  order by 1");
+
+			SevCargaAbonoDTO cargaAbonoDTO = new SevCargaAbonoDTO();
+
+			while (rs.next()) {
+				contador=contador+1;
+				System.out.println("linea: "+contador);
+				cargaAbonoDTO = new SevCargaAbonoDTO(rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4),
+						rs.getString(5), rs.getString(6), rs.getString(7), rs.getString(8), rs.getString(9),
+						rs.getString(10), rs.getString(11), rs.getString(12), rs.getString(13), rs.getString(14),
+						rs.getString(15), rs.getString(16));
+
+				String val_fecha_fin = "NO";
+
+				int NESTATUS = Integer.parseInt(cargaAbonoDTO.getnEstatus());
+				int NMAX_INTENTOS = Integer.parseInt(cargaAbonoDTO.getnMaxIntentos());
+
+				
+
+				if (cargaAbonoDTO.getnCargoAbono().contentEquals("ABN")) {
+
+					lstQuerys.addAll(processInfo.deposito(idAplicacion, urlSycDep, token, cargaAbonoDTO.getNidCA(),
+							cargaAbonoDTO.getnTarjeta(), cargaAbonoDTO.getnCuenta(), cargaAbonoDTO.getnCliente(),
+							cargaAbonoDTO.getnMonto(), cargaAbonoDTO.getnMotivo(), cargaAbonoDTO.getnIntento(),
+							NESTATUS, NMAX_INTENTOS, val_fecha_fin, cargaAbonoDTO.getvPeriodicidad(),
+							cargaAbonoDTO.getnClaseMov(), cargaAbonoDTO.getvTipoPoliza(), cargaAbonoDTO.getnProducto(),
+							cargaAbonoDTO.getnCargoAbono()));
+
+				} else if (cargaAbonoDTO.getnCargoAbono().contentEquals("CRG")) {
+
+					lstQuerys.addAll(processInfo.retiro(idAplicacion, urlSycRetiro, token, cargaAbonoDTO.getNidCA(),
+							cargaAbonoDTO.getnTarjeta(), cargaAbonoDTO.getnCuenta(), cargaAbonoDTO.getnCliente(),
+							cargaAbonoDTO.getnMonto(), cargaAbonoDTO.getnMotivo(), cargaAbonoDTO.getnIntento(),
+							NESTATUS, NMAX_INTENTOS, val_fecha_fin, cargaAbonoDTO.getvPeriodicidad(),
+							cargaAbonoDTO.getnClaseMov(), cargaAbonoDTO.getvTipoPoliza(), cargaAbonoDTO.getnProducto(),
+							cargaAbonoDTO.getnCargoAbono()));
+
+				} else {
+					System.out.println("No hay configuracion del tipo de movimiento, cargo o abono");
+				}
+
+			}
+			
+			System.out.println("Termina de leer y comienza hacer los update");
+			LOGGER.log(Level.INFO, "Termina de leer y comienza hacer los update");
+			try (Statement stmtTCargoAbono = conn.createStatement();
+					Statement stmtBitacoraCargo = conn.createStatement();
+					Statement stmtMovimientosAcla = conn.createStatement();
+					Statement stmtCuentasMovim = conn.createStatement();
+					Statement stmtMovRestoLaat = conn.createStatement();
+					Statement stmtSetCobroMov = conn.createStatement();
+					Statement stmtCtrxAj = conn.createStatement();
+					Statement stmtCobroComPoliza = conn.createStatement()) {
+				conn.setAutoCommit(false);
+				for (ObjQuery obj : lstQuerys) {
+					if (obj.getTable().equals(Constants.TABLA_CTRX_AJ)) {
+						stmtCtrxAj.addBatch(obj.getQuery());
+					} else if (obj.getTable().equals(Constants.TABLA_TCARGO_ABONOS)) {
+						stmtTCargoAbono.addBatch(obj.getQuery());
+					} else if (obj.getTable().equals(Constants.TABLA_CUENTAS_MOVIMIENTOS)) {
+						stmtCuentasMovim.addBatch(obj.getQuery());
+					} else if (obj.getTable().equals(Constants.TABLA_MOV_RESTO_LAAT)) {
+						stmtMovRestoLaat.addBatch(obj.getQuery());
+					} else if (obj.getTable().equals(Constants.TABLA_SET_COBRO_COMISIONES_POLIZAS)) {
+						stmtCobroComPoliza.addBatch(obj.getQuery());
+					} else if (obj.getTable().equals(Constants.TABLA_SET_MOVIMIENTOS_ACLARACIONES)) {
+						stmtMovimientosAcla.addBatch(obj.getQuery());
+					} else if (obj.getTable().equals(Constants.TABLA_SEV_TBITACORA_CARGOSABONOS)) {
+						stmtBitacoraCargo.addBatch(obj.getQuery());
+					} else {
+						stmtSetCobroMov.addBatch(obj.getQuery());
+					}
+				}
+
+				stmtCtrxAj.executeBatch();
+				stmtTCargoAbono.executeBatch();
+				stmtCuentasMovim.executeBatch();
+				stmtMovRestoLaat.executeBatch();
+				stmtCobroComPoliza.executeBatch();
+				stmtMovimientosAcla.executeBatch();
+				stmtBitacoraCargo.executeBatch();
+				stmtSetCobroMov.executeBatch();
+
+				System.out.println("################ Numero de Lineas: " + lstQuerys.size());
+				LOGGER.log(Level.INFO, "Movimientos registrados: " + lstQuerys.size());
+
+				conn.commit();
+
+			} catch (SQLException e) {
+				e.printStackTrace();
+				LOGGER.log(Level.SEVERE, "Error al insertar movimientos -> " + e);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
 }
--- ValeElectronicoCargoAbono/src/main/java/com/sodexo/mx/dbconnectios/CreateConnectionDB.java
+++ ValeElectronicoCargoAbono/src/main/java/com/sodexo/mx/dbconnectios/CreateConnectionDB.java
@@ -1,42 +1,57 @@
 package com.sodexo.mx.dbconnectios;
 
+import java.io.FileInputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.Properties;
+import java.util.logging.Logger;
 
 public class CreateConnectionDB {
+	private final static Logger LOGGER  = Logger.getLogger("log");
 	
+	public static final String DATABASE_USER = "user";
+	public static final String DATABASE_PASSWORD = "password";
+
 	public void testQuery() {
+
 		try {
-			Connection conn =openConnectionOracle();
-			
+			Connection conn = openConnectionOracle();
+
 			String query = "select VCLAVE_MOV from SEV.SEV_TCARGOS_ABONOS where NID_CA = ?";
-            PreparedStatement st = conn.prepareStatement(query);
-            st.setInt(1,22594);
-            ResultSet rs = st.executeQuery();
-            while (rs.next()) {
-            	System.out.println("respuesta  " +rs.getString("VCLAVE_MOV"));
-            }
-		}catch(Exception ex) {
-			
+			PreparedStatement st = conn.prepareStatement(query);
+			st.setInt(1, 22594);
+			ResultSet rs = st.executeQuery();
+			while (rs.next()) {
+				System.out.println("respuesta  " + rs.getString("VCLAVE_MOV"));
+			}
+		} catch (Exception ex) {
+
 		}
 	}
-	
-	
-	public static Connection openConnectionOracle() throws Exception {
-		
-        System.out.println("++++++++++++++  OPEN CONN");
-        String myDriver = "oracle.jdbc.OracleDriver";
-        //String myUrl = "jdbc:oracle:thin:@192.168.1.103:1521/xe";
-        //String myUrl = "jdbc:oracle:thin:@34.122.188.80:1521/xe";
-        String myUrl = "jdbc:oracle:thin:@10.52.21.4:1521/SEVDES";
-        Class.forName(myDriver);
-        //Connection conn = DriverManager.getConnection(myUrl, "SISAMEX", "welcome1");
-        Connection conn = DriverManager.getConnection(myUrl, "SEV", "SEV001");
-        System.out.println("++++++++++++++  END OPEN CONN");
-        return conn;
 
-    }
+	public Connection openConnectionOracle() throws Exception {
+		Properties mainProperties = new Properties();	   
+		   FileInputStream file;	   
+		   String path = "../config/config.properties";	   
+		   file = new FileInputStream(path);
+		   mainProperties.load(file);
+		   file.close();
+		   String usuario_db = mainProperties.getProperty("usuario_db");
+		   String pass_db = mainProperties.getProperty("pass_db");
+		   String url_db = mainProperties.getProperty("url_db");
+		   mainProperties.put(DATABASE_USER, usuario_db);
+		   mainProperties.put(DATABASE_PASSWORD, pass_db);
+
+		System.out.println("++++++++++++++  OPEN CONN");
+		String myDriver = "oracle.jdbc.OracleDriver";
+		String myUrl = "jdbc:oracle:thin:@10.52.21.4:1521/SEVDES";
+		Class.forName(myDriver);
+		Connection conn = DriverManager.getConnection(myUrl, mainProperties);
+		System.out.println("++++++++++++++  END OPEN CONN");
+		return conn;
+
+	}
 
 }
